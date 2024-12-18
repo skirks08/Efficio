c9b31d3b0822a10972057342980090ce5afacc86
Michael Skirka <skirkamp@hotmail.com>
2024-12-18 17:59:57 +0000

    updated store js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from '';
+import tasksReducer from './tasksSlice';
+import userPreferencesReducer from './userPreferencesSlice';
+import uiStateReducer from './uiStateSlice';
 
 export const store = configureStore({
     reducer: {
         tasks: tasksReducer,
+        userPreferences: userPreferencesReducer,
+        uiState: uiStateReducer,
     },
 });

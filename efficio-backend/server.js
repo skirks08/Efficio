1b67010c5000c0d6af7742a8dc214bcc254b9772
Michael Skirka <skirkamp@hotmail.com>
2025-01-09 15:26:05 +0000

    added fetch tasks to backend

--- efficio-backend/server.js
+++ efficio-backend/server.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+let tasks = [];
+
+// Fetch tasks
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});

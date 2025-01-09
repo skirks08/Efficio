633ac8cec873f386e82dad8da2fca5b3ee24dfb2
Michael Skirka <skirkamp@hotmail.com>
2025-01-09 18:01:14 +0000

    updated store.js and imports in store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from './redux/actions/tasksActions';
+import { configureStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './redux/reducers';
 import userPreferencesReducer from './redux/userPreferencesSlice';
 import uiStateReducer from './redux/uiStateSlice';
 
-export const store = configureStore({
-    reducer: {
-        tasks: tasksReducer,
-        userPreferences: userPreferencesReducer,
-        uiState: uiStateReducer,
-    },
-});
+const store = configureStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;

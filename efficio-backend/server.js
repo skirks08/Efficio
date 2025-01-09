58dca7b576ee75e2cee1b031cabf7d6359b48f8c
Michael Skirka <skirkamp@hotmail.com>
2025-01-09 15:28:41 +0000

    added 'add task' section to server in backend

--- efficio-backend/server.js
+++ efficio-backend/server.js
@@ -13,3 +13,11 @@ let tasks = [];
 app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
+
+// Add task
+
+app.post('/tasks', (req, res) => {
+    const newTask = { id: Date.now(), ...req.body };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+})

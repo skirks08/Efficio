4bbb7d064ee73abbec7516eb112989cbbc8c9e65
Michael Skirka <skirkamp@hotmail.com>
2025-01-09 18:05:05 +0000

    updated index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import { store } from './store';
 import { Provider } from 'react-redux';
 //import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

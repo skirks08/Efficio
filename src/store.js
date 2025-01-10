0ec795adc3127dc44d4c4e2f6fde073204ba8171
Michael Skirka <skirkamp@hotmail.com>
2025-01-10 19:45:53 +0000

    moved ui state and user preferences files into redux folder

--- src/store.js
+++ src/store.js
@@ -2,8 +2,8 @@ import { configureStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './redux/reducers';
-import userPreferencesReducer from './redux/userPreferencesSlice';
-import uiStateReducer from './redux/uiStateSlice';
+import userPreferencesReducer from './redux/reducers/userPreferencesSlice';
+import uiStateReducer from './redux/reducers/uiStateSlice';
 
 const store = configureStore(
     rootReducer,

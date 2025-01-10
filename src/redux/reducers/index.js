1e69ebb23bdab86f01d3112ae2566fba1637dd57
Michael Skirka <skirkamp@hotmail.com>
2025-01-10 19:52:22 +0000

    added uistate and userPreferences imports in reducers index file

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import tasksReducer from "./tasksReducer";
+import userPreferencesReducer from './userPreferencesSlice';
+import uiStateReducer from './uiStateSlice';
 
 // Combine reducers into Root reducer
 

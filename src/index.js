65bd2a51049daf336192694e289cbfc29e9e9f9e
Michael Skirka <skirkamp@hotmail.com>
2025-01-10 22:16:30 +0000

    updated index.js to align with updated versions of react and redux

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import store from './store';
 import { Provider } from 'react-redux';
 //import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
